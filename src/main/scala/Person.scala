0867015bd88a16d6a56ae3770349d7dbcce0c6b1
[utkarsha] <utkarsha.Musmade@knoldus.in>
2019-07-12 23:19:52 +0530

    project Added

---
+++ src/main/scala/Person.scala
@@ -0,0 +1,20 @@
+class Person(val name: String, val age: Int) extends Ordered[Person] {
+
+
+  override def compare(that: Person): Int = {
+    if (this.name == that.name) {
+      if (this.age < that.age) 1 else 0
+    }
+    else if (this.name.length < this.name.length) 1 else 0
+
+  }
+
+}
+
+object Person extends App {
+  val personOne = new Person("Utkarsha", 21)
+  val personTwo = new Person("Utkarsha", 22)
+  println(personOne.compare(personTwo))
+}
+
+
